fd0ee92157a526158768562496bc69160ce447ab
CISCO\randash <randash@cisco.com>
2018-10-30 08:47:06 +0530

    Added try catch blocks for error handling

--- currency-convert.js
+++ currency-convert.js
@@ -16,11 +16,17 @@ const axios = require('axios');
 // };
 
 const getExchangeRate = async (from, to) => {
-  var response = await axios.get('http://data.fixer.io/api/latest?access_key=fe2c24113f46ef2c0d7191609a9d6703&format=1');
-  var euro = 1 / response.data.rates[from];
-  var rate = euro * response.data.rates[to];
-
-  return rate;
+  try{
+    var response = await axios.get('http://data.fixer.io/api/latest?access_key=fe2c24113f46ef2c0d7191609a9d6703&format=1');
+    var euro = 1 / response.data.rates[from];
+    var rate = euro * response.data.rates[to];
+    if(isNaN(rate)){
+      throw new Error();
+    }
+    return rate;
+  }catch(e){
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}.`);
+  }
 };
 
 // const getCountries = (countryCode) => {
@@ -30,16 +36,44 @@ const getExchangeRate = async (from, to) => {
 // };
 
 const getCountries = async (countryCode) => {
-  var response= await axios.get(`https://restcountries.eu/rest/v2/currency/${countryCode}`);
-  return response.data.map((country) => country.name);
+  try{
+    var response= await axios.get(`https://restcountries.eu/rest/v2/currency/${countryCode}`);
+    return response.data.map((country) => country.name);
+  }catch(err){
+    throw new Error(`Unable to get countries that use ${countryCode}.`);
+  }
+
 };
 
-getExchangeRate('USD','INR').then((rate) => {
-  console.log(rate);
-}).catch((err)=>{
-  console.log(err);
-});
+// getExchangeRate('USD','INR').then((rate) => {
+//   console.log(rate);
+// }).catch((err)=>{
+//   console.log(err);
+// });
+//
+// getCountries('INR').then((countries) => {
+//   console.log(countries);
+// });
 
-getCountries('INR').then((countries) => {
-  console.log(countries);
+// const convertCurrency = (from, to, amount) => {
+//   let convertedAmount;
+//   return getExchangeRate(from, to).then((rate) => {
+//     convertedAmount = (amount * rate).toFixed(2);
+//     return getCountries(to);
+//   }).then((countries) => {
+//     return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend them in the following countries :${countries.join(', ')}`;
+//   });
+// };
+
+const convertCurrency = async (from, to, amount) => {
+  var rate = await getExchangeRate(from, to);
+  var convertedAmount = (amount * rate).toFixed(2);
+  var countries = await getCountries(to);
+  return `${amount} ${from} is worth ${convertedAmount} ${to}. You can spend them in the following countries :${countries.join(', ')}`;
+};
+
+convertCurrency('USD','INR',20).then((message) => {
+  console.log(message)
+}).catch((err) => {
+  console.log(err);
 });
